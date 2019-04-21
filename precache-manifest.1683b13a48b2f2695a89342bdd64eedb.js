5906d0613a254c910d71c6295a431830abcace86
ropali <ropali68@gmail.com>
2019-04-21 21:13:10 +0530

    Updates

---
+++ precache-manifest.1683b13a48b2f2695a89342bdd64eedb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbb311601833dc31988bda93784319ca",
+    "url": "/portfolio/static/media/user.dbb31160.png"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "200f3771b0f157a40c10",
+    "url": "/portfolio/static/js/main.cf9aa6e7.chunk.js"
+  },
+  {
+    "revision": "d678ad90cb672ab2ed30",
+    "url": "/portfolio/static/js/2.4e935125.chunk.js"
+  },
+  {
+    "revision": "200f3771b0f157a40c10",
+    "url": "/portfolio/static/css/main.4c868c31.chunk.css"
+  },
+  {
+    "revision": "2b5dd9d86b32e6093404a6ebee5b3896",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ropali/portfolio/precache-manifest.0f4426dda13124e564b8a0103e51eb9b.js"
+  "/portfolio/precache-manifest.1683b13a48b2f2695a89342bdd64eedb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ropali/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
